bde991ac1d3dfc308ec64592c721c917bcf50bd9
Siddarth <sid@MacBook-Pro.local>
2020-07-13 18:13:30 +0530

    refactor(tests): cleanup url names

--- test/unit/botRouter.spec.ts
+++ test/unit/botRouter.spec.ts
@@ -3,37 +3,36 @@ import { BotRouter } from '../../src/bots/botrouter'
 
 // ----------------------------------------------------------------
 // --------------------------- setup ------------------------------
-const times = Math.ceil(Math.random() * 10)
-const bots1 = Array(times).fill(1).map((x, i) => {
+const bots1 = Array(10).fill(1).map((x, i) => {
   return new Bot(`user${i}`, `pass${i}`)
 })
-const bots2 = Array(times).fill(1).map((x, i) => {
+const bots2 = Array(8).fill(1).map((x, i) => {
   return new Bot(`altuser${i}`, `altpass${i}`)
 })
 const botRouter = new BotRouter({
-  'url1': bots1,
-  'url2': [],
-  'url2/subdomain/': bots2,
+  'https://example.com/domain-1': bots1,
+  'https://example.com/domain-2': [],
+  'https://example.com/subdomain/': bots2,
 })
 // ------------------------- end setup ----------------------------
 // ----------------------------------------------------------------
 
 test('create botRouter with multiple bots', () => {
   const br = new BotRouter({
-    'url1': bots1,
-    'url2': [],
-    'url2/subdomain/': bots2,
+    'https://example.com/domain-1': bots1,
+    'https://example.com/domain-2': [],
+    'https://example.com/subdomain/': bots2,
   })
   expect(br).toBeDefined()
 })
 
 test('get bot when bots is not empty', () => {
-  const nonEmptyBot = botRouter.getBotForUrl('url1')
+  const nonEmptyBot = botRouter.getBotForUrl('https://example.com/domain-1')
   expect(nonEmptyBot).toBeDefined()
 })
 
 test('get bot when bots is empty', () => {
-  const noBot = botRouter.getBotForUrl('url2')
+  const noBot = botRouter.getBotForUrl('https://example.com/domain-2')
   expect(noBot).toBeUndefined()
 })
 
